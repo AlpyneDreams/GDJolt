3383e80de52c7b09a1e7273f7dbc84e6c53e2437
Alpyne <alpyne.dreams@gmail.com>
2022-10-11 06:47:57 -0700

    Add Jolt as a dependency

--- gdjolt.cpp
+++ gdjolt.cpp
@@ -1,8 +1,26 @@
 #include "gdjolt.h"
 
+using Jolt = JoltPhysicsServer3D;
 using Base = GodotPhysicsServer3D;
 
-RID JoltPhysicsServer3D::body_create() {
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+
+RID Jolt::body_create() {
+    using namespace JPH;
+    //Bodies().CreateBody(...);
     printf("Body created!\n");
+
     return Base::body_create();
+}
+
+void Jolt::free(RID p_rid) {
+    printf("Freeing RID %lu\n", p_rid.get_id());
+
+    // Check each owner
+    if (own_bodies.owns(p_rid)) {
+        Physics.GetBodyInterfaceNoLock().DestroyBody(get_body_id(p_rid));
+        own_bodies.free(p_rid);
+    }
+
+    return Base::free(p_rid);
 }
--- gdjolt.h
+++ gdjolt.h
@@ -4,12 +4,48 @@
 
 #include "servers/physics_3d/godot_physics_server_3d.h"
 
-
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Body/Body.h>
+#include <Jolt/Physics/Body/BodyInterface.h>
 
 class JoltPhysicsServer3D : public GodotPhysicsServer3D {
     GDCLASS(JoltPhysicsServer3D, GodotPhysicsServer3D);
 
+    mutable RID_PtrOwner<JPH::Body, true> own_bodies;
+
+public:
+
+	/* SHAPE API */
+
+	/* SPACE API */
+
+	/* AREA API */
+
+	/* BODY API */
+    
     virtual RID body_create() override;
+
+	/* SOFT BODY */
+
+	/* JOINT API */
+
+	/* MISC */
+
+	virtual void free(RID p_rid) override;
+
+private:
+    JPH::PhysicsSystem Physics;
+
+    // Internals
+
+    const JPH::BodyID& get_body_id(RID p_rid) const {
+        return own_bodies.get_or_null(p_rid)->GetID();
+    }
+
+    const JPH::BodyInterface& Bodies() {
+        return Physics.GetBodyInterfaceNoLock();
+    }
 
 };
 
